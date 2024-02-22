7ca3cdd1700190c430117801c2a151cc587d00ef
Aleksandr Ataev <onlyaa94@mail.ru>
2024-02-22 13:18:15 +0300

    Initial Commit

---
+++ QuizXguide/QuizXguideApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizXguideApp.swift
+//  QuizXguide
+//
+//  Created by Aleksandr Ataev on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizXguideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
